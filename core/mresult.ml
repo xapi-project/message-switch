abb858b1b96285f7796aad5a91f827a9e6c2ef0e
Ben Anson <ben.anson@citrix.com>
2021-07-16 10:00:45 +0100

    make format
    Signed-off-by: Ben Anson <ben.anson@citrix.com>

--- core/mresult.ml
+++ core/mresult.ml
@@ -12,28 +12,35 @@
  * GNU Lesser General Public License for more details.
  *)
 
-type ('a, 'b) result = [
-| `Ok of 'a 
-| `Error of 'b
-]
+type ('a, 'b) result = [`Ok of 'a | `Error of 'b]
 
 let bind v f = match v with `Ok v -> f v | `Error _ as e -> e
+
 let ( >>= ) = bind
 
 let return x = `Ok x
+
 let ok = return
 
 let all xs =
   let rec loop acc = function
-  | [] -> return (List.rev acc)
-  | `Ok x :: xs -> loop (x :: acc) xs
-  | `Error x :: _ -> `Error x in
+    | [] ->
+        return (List.rev acc)
+    | `Ok x :: xs ->
+        loop (x :: acc) xs
+    | `Error x :: _ ->
+        `Error x
+  in
   loop [] xs
 
 let get_ok = function
-| `Ok x -> x
-| `Error _ -> raise (Invalid_argument "get_ok encountered an `Error")
+  | `Ok x ->
+      x
+  | `Error _ ->
+      raise (Invalid_argument "get_ok encountered an `Error")
 
 let get_error = function
-| `Error x -> x
-| `Ok _ -> raise (Invalid_argument "get_error encountered an `Ok")
+  | `Error x ->
+      x
+  | `Ok _ ->
+      raise (Invalid_argument "get_error encountered an `Ok")
